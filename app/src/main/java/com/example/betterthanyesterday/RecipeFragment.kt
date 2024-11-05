ab24542bae85bc890d702c8950e475700cc36c6b
Jinhyeok <jinhyeok000117@gmail.com>
2024-11-06 00:34:10 +0900

    Add recipe

--- app/src/main/java/com/example/betterthanyesterday/RecipeFragment.kt
+++ app/src/main/java/com/example/betterthanyesterday/RecipeFragment.kt
@@ -5,19 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.betterthanyesterday.databinding.FragmentRecipeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RecipeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecipeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -33,20 +27,14 @@ class RecipeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+        val binding = FragmentRecipeBinding.inflate(inflater, container, false)
+        binding.btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_recipeFragment_to_RecipeAddFragment)
+        }
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecipeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             RecipeFragment().apply {
